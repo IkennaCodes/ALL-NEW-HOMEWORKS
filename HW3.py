8d4244326a8e51bc97fd8a8f2d3826615a0726c9
IkennaCodes <ikeandadanjoku@gmail.com>
2025-03-09 12:39:41 +0000

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,7 @@
+P = int(input("Enter principe amount"))
+R = int(input("Enter the rate of interest"))
+T = int(input("Enter the time period"))
+
+SI = (P * R * T) / 100   
+
+print ("The simple interest = £", SI)
